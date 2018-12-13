4320edc2b1c7b78e963e5249e2a726f6833263ed
Michael <loukerismichalis@gmail.com>
2018-12-13 02:39:55 +0200

    void function shouldnt return anything

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1030,8 +1030,6 @@ schedtune_boostgroup_init(struct schedtune *st, int idx)
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	boost_slots_init(st);
 #endif // CONFIG_DYNAMIC_STUNE_BOOST
-
-	return 0;
 }
 
 static struct cgroup_subsys_state *
